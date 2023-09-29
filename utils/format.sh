30d29cf1c28c9e5cbb3134728d6947102d8cb414
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-29 20:23:30 +0700

    Fix utils/format script to format only code in src folder

--- utils/format.sh
+++ utils/format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-filepaths=$(find . -type f \( -name "*.cpp" -or -name "*.hpp" \))
+filepaths=$(find src -type f \( -name "*.cpp" -or -name "*.hpp" \))
 for filepath in $filepaths; do
   clang-format -i "${filepath}"
 done
