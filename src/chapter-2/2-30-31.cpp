bf3479828749ef0d3791549f744f2ff32006d306
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-13 17:57:12 +0700

    Add solutions for tasks 2-30 & 2-31

---
+++ src/chapter-2/2-30-31.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  // 2.30
+  const int v2 = 0; // low-level const
+  int v1 = v2; // no const
+  int *p1 = &v1, &r1 = v1; // no const
+  const int *p2 = &v2, *const p3 = &v1, &r2 = v2;
+  // ^ p2 - upper-level const, p3 - low-level const, r2 - upper-level const
+  std::cout << r2 << ' ';
+
+  // 2.31
+  r1 = v2; // ok
+  // p1 = p2; // wrong conversion from const int* to int*
+  // p1 = p3; // wrong conversion from const int* to int*
+  p2 = p1; // ok, const int* = int*
+  p2 = p3; // ok, const int* = const int* const
+  std::cout << *p2 << std::endl;
+  return 0;
+}
