7efbbdd7835b0393bf58f91e3d3e7469a8c2f023
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-20 20:04:28 +0700

    Add solution for task 3-1

---
+++ src/chapter-3/3-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+  cout << "Wow it's so cool I can use cout instead of std::cout\n";
+  cout << "Btw I'm too lazy to rewrite all tasks with \"using\"s\n";
+  return 0;
+}
