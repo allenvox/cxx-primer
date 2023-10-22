ea7b161944b26474fa340f5fe2820f2fc84b27b2
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-22 12:17:31 +0700

    Format code

--- src/chapter-3/3-4.cpp
+++ src/chapter-3/3-4.cpp
@@ -5,11 +5,11 @@ int main() {
   std::string s1, s2;
   getline(std::cin, s1);
   getline(std::cin, s2);
-  if(s1 == s2) {
+  if (s1 == s2) {
     std::cout << "Strings are equal\n";
   } else {
     std::cout << "Strings are not equal, ";
-    if(s1 > s2) {
+    if (s1 > s2) {
       std::cout << "#1";
     } else {
       std::cout << "#2";
@@ -17,11 +17,11 @@ int main() {
     std::cout << " is lexicographically bigger (alphabetically first)\n";
   }
 
-  if(s1.length() == s2.length()) {
+  if (s1.length() == s2.length()) {
     std::cout << "Strings have same length";
   } else {
     std::cout << "Strings have different lengths, ";
-    if(s1.length() > s2.length()) {
+    if (s1.length() > s2.length()) {
       std::cout << "#1";
     } else {
       std::cout << "#2";
