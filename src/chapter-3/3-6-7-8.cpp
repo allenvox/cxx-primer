a0c543e4cb74186b85c5975c71863f2dd435ae2d
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-22 14:31:19 +0700

    Format code

--- src/chapter-3/3-6-7-8.cpp
+++ src/chapter-3/3-6-7-8.cpp
@@ -3,11 +3,11 @@
 int main() {
   std::string str("hello");
   std::cout << str << '\n';
-  for(char &c : str) {
+  for (char &c : str) {
     c = 'X';
   }
   std::cout << str << '\n';
-  
+
   // 1. with while-loop it's not clear how many iterations we'll do
   //    and also can get segfault if out of bounds
   // 2. for-loop is ok
