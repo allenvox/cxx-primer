27fbc42a371a9216264d3addc46d9ade157a3609
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-30 18:30:05 +0700

    Add solution for tasks 3-27 & 3-28

---
+++ src/chapter-3/3-27-28.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+// 3.28
+std::string sa[10]; // empty strings
+int ia[10]; // zeroes
+
+std::size_t txt_size() {
+  return 5;
+}
+
+int main() {
+  // 3.27
+  unsigned buf_size = 1024;
+  int ia11[buf_size]; // ok
+  int ia12[4 * 7 - 14]; // ok
+  int ia13[txt_size()]; // ok
+  //char st[11] = "fundamental"; // wrong - size needs to be 12 (with \0)
+
+  // 3.28
+  for(auto s : sa) {
+    std::cout << s << ' ';
+  }
+  std::cout << '\n';
+
+  for(auto i : ia) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+
+  std::string sa2[10]; // empty strings
+  for(auto s : sa2) {
+    std::cout << s << ' ';
+  }
+  std::cout << '\n';
+
+  int ia2[10]; // UNDEFINED (INT MIN/MAX, 0, RANDOM ABSOLUTELY)
+  for(auto i : ia2) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+
+  // 3.28
+
+  return 0;
+}
