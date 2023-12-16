387aca095b6b26581bf6c89747add85f5218ed84
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-17 01:42:07 +0700

    Add solution for tasks 4-1 & 4-2

---
+++ src/chapter-4/4-1-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  // 4.1
+  std::cout << 5 + 10 * 20/2 << '\n'; // 5 + ((10 * 20) / 2)
+
+  // 4.2
+  // (a) (* (vec.begin()))
+  // (b) (* (vec.begin() + 1))
+  return 0;
+}
